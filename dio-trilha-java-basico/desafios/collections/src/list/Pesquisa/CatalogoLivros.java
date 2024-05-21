02ec878bc531a984a82c15da1fb37f1c3827d6f6
gabizelaa <madonamia68@gmail.com>
2024-05-21 19:19:04 -0300

    #

---
+++ dio-trilha-java-basico/desafios/collections/src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,77 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    //atributos
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if (l.getAutor().equalsIgnoreCase(autor)){
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal){
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+    return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Microsserviços Prontos Para a Produção", "Susan J. Fowler", 2017);
+        catalogoLivros.adicionarLivro("Java Guia do Programador", "Peter Jandl Junior", 2021);
+        catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2009);
+        catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2012);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Robert C. Martin"));
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor Inexistente"));
+
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2010, 2022));
+
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2025, 2030));
+
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Java Guia do Programador"));
+
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Título Inexistente"));
+    }
+}
