16461b83de4b47f9b2b82b475d730b8579d65e6f
gabizelaa <madonamia68@gmail.com>
2024-05-21 19:31:08 -0300

    #

---
+++ dio-trilha-java-basico/desafios/collections/src/list/Pesquisa/SomaNumeros.java
@@ -0,0 +1,80 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+
+    //atributos
+
+    private List<Integer> numeroList;
+
+    public SomaNumeros() {
+        this.numeroList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        this.numeroList.add(numero);
+    }
+
+    public int calcularSoma(){
+        int soma = 0;
+        for(Integer numero : numeroList){
+            soma += numero;
+            return soma;
+        }
+    }
+
+    public int encontrarMaiorNumero(){
+        int maiorNumero = Integer.MAX_VALUE;
+        if (!numeroList.isEmpty()) {
+            for (Integer numero : numeroList) {
+                if (numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        }
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = Integer.MAX_VALUE;
+        if (!numeroList.isEmpty()) {
+            for (Integer numero : numeroList) {
+                if (numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+            return menorNumero;
+        }
+    }
+
+    public void exibirNumeros() {
+        if (!numeroList.isEmpty()) {
+            System.out.println(this.numeroList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-2);
+        somaNumeros.adicionarNumero(10);
+
+        System.out.println("Números adicionados:");
+        somaNumeros.exibirNumeros();
+
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+    }
+}
+}
+
