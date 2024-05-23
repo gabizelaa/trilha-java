69c64530c88a969c15b90fdf8d10cca0e5bf4c43
gabizelaa <madonamia68@gmail.com>
2024-05-23 15:07:19 -0300

    adicionando exercício AgendaContatos com o uso de Map

---
+++ dio-trilha-java-basico/desafios/collections/src/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,60 @@
+package map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome){
+        Integer numeroPorNome = null;
+        if(!agendaContatoMap.isEmpty()){
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("João", 5665);
+        agendaContatos.adicionarContato("Carlos", 1111111);
+        agendaContatos.adicionarContato("Ana", 654987);
+        agendaContatos.adicionarContato("Maria", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria");
+        agendaContatos.exibirContatos();
+
+        String nomePesquisa = "João";
+        Integer numeroPesquisa = agendaContatos.pesquisarPorNome("João");
+        System.out.println("Número de telefone de " + nomePesquisa + ": " + numeroPesquisa);
+
+        String nomePesquisaNaoExistente = "Paula";
+        Integer numeroPesquisaNaoExistente = agendaContatos.pesquisarPorNome(nomePesquisaNaoExistente);
+        System.out.println("Número de telefone de " + nomePesquisaNaoExistente + ": " + numeroPesquisaNaoExistente);
+
+    }
+}
