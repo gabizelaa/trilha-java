a9ecb64e8b3a5eadb5046e8f1833e73db6131907
gabizelaa <madonamia68@gmail.com>
2024-05-23 15:31:12 -0300

    adicionando exercício Dicionario com o uso de Map

---
+++ dio-trilha-java-basico/desafios/collections/src/map/OperacoesBasicas/Dicionario.java
@@ -0,0 +1,56 @@
+package map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+
+    private Map<String, String> dicionarioMap;
+
+    public Dicionario() {
+        this.dicionarioMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definicao){
+        dicionarioMap.put(palavra, definicao);
+    }
+
+    public void removerPalavra(String palavra){
+        if(!dicionarioMap.isEmpty()){
+            dicionarioMap.remove(palavra);
+        }
+    }
+
+    public void exibirPalavras(){
+        System.out.println(dicionarioMap);
+    }
+
+    public String pesquisarPorPalavra(String palavra){
+        String definicaoPorNome = null;
+        if(!dicionarioMap.isEmpty()){
+            definicaoPorNome = dicionarioMap.get(palavra);
+        }
+        return definicaoPorNome;
+    }
+
+    public static void main(String[] args) {
+
+        Dicionario dicionario = new Dicionario();
+
+        dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
+        dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
+        dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
+
+        dicionario.exibirPalavras();
+
+        String definicaoJava = dicionario.pesquisarPorPalavra("java");
+        System.out.println("Definição da linguagem 'java': " + definicaoJava);
+
+        String definicaoCSharp = dicionario.pesquisarPorPalavra("csharp");
+        System.out.println(definicaoCSharp);
+
+        dicionario.removerPalavra("typescript");
+        dicionario.exibirPalavras();
+    }
+
+}
