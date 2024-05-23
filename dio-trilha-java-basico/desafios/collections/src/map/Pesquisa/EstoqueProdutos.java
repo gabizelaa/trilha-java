1d7bdab0b98312b89524a9e976fe2dc0c0665f18
gabizelaa <madonamia68@gmail.com>
2024-05-23 19:35:07 -0300

    adicionando exercício EstoqueProdutos com o uso de Map

---
+++ dio-trilha-java-basico/desafios/collections/src/map/Pesquisa/EstoqueProdutos.java
@@ -0,0 +1,46 @@
+package map.Pesquisa;
+
+import set.Ordenacao.Produto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EstoqueProdutos {
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(cod, nome, preco, quantidade));
+    }
+
+    public void exibirProdutos() {
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        double valorTotalEstoque = 0;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for(Produto p : estoqueProdutosMap.values()){
+                if(p.getPreco() > maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+}
