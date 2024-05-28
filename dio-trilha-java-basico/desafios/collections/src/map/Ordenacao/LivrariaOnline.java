21275cdd90f830dab16b97e7b56763c65b3c9b51
gabizelaa <madonamia68@gmail.com>
2024-05-28 18:42:29 -0300

    adicionando exercício LivrariaOnline com o uso de Map

---
+++ dio-trilha-java-basico/desafios/collections/src/map/Ordenacao/LivrariaOnline.java
@@ -0,0 +1,101 @@
+package map.Ordenacao;
+
+import java.util.*;
+
+public class LivrariaOnline {
+
+    private Map<String, Livro> livroMap;
+
+    public LivrariaOnline() {
+        this.livroMap = new HashMap<>();
+    }
+
+    public void adicionarLivro(String link, Livro livro){
+        livroMap.put(link, livro);
+    }
+
+    public void removerLivro(String titulo) {
+        List<String> chavesRemover = new ArrayList<>();
+        for (Map.Entry<String, Livro> entry : livroMap.entrySet()) {
+            if (entry.getValue().getTitulo().equalsIgnoreCase(titulo)) {
+                chavesRemover.add(entry.getKey());
+            }
+        }
+        for (String chave : chavesRemover) {
+            livroMap.remove(chave);
+        }
+    }
+
+    public Map<String, Livro> exibirLivrosOrdenadosPorPreco() {
+        List<Map.Entry<String, Livro>> livrosParaOrdenarPorPreco = new ArrayList<>(livroMap.entrySet());
+
+        Collections.sort(livrosParaOrdenarPorPreco, new ComparatorPorPreco());
+
+        Map<String, Livro> livrosOrdenadosPorPreco = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorPreco) {
+            livrosOrdenadosPorPreco.put(entry.getKey(), entry.getValue());
+        }
+
+        return livrosOrdenadosPorPreco;
+    }
+
+    public Map<String, Livro> pesquisarLivrosPorAutor(String autor) {
+        Map<String, Livro> livrosPorAutor = new LinkedHashMap<>();
+        for (Map.Entry<String, Livro> entry : livroMap.entrySet()) {
+            Livro livro = entry.getValue();
+            if (livro.getAutor().equals(autor)) {
+                livrosPorAutor.put(entry.getKey(), livro);
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public Livro obterLivroMaisCaro(){
+        Livro livroMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!livroMap.isEmpty()) {
+            for(Livro l : livroMap.values()){
+                if(l.getPreco() > maiorPreco){
+                    livroMaisCaro = l;
+                }
+            }
+        }
+        return livroMaisCaro;
+    }
+
+    public Livro obterLivroMaisBarato(){
+        Livro livroMaisBarato = null;
+        double menorPreco = Double.MAX_VALUE;
+        if (!livroMap.isEmpty()) {
+            for(Livro l : livroMap.values()){
+                if(l.getPreco() > menorPreco){
+                    livroMaisBarato = l;
+                }
+            }
+        }
+        return livroMaisBarato;
+    }
+
+    public static void main(String[] args) {
+        LivrariaOnline livrariaOnline = new LivrariaOnline();
+
+        livrariaOnline.adicionarLivro("https://amzn.to/3EclT8Z", new Livro("1984", "George Orwell", 50));
+        livrariaOnline.adicionarLivro("https://amzn.to/47Umiun", new Livro("A Revolução dos Bichos", "George Orwell", 7.05));
+        livrariaOnline.adicionarLivro("https://amzn.to/3L1FFI6", new Livro("Caixa de Pássaros - Bird Box: Não Abra os Olhos", "Josh Malerman", 19.99));
+        livrariaOnline.adicionarLivro("https://amzn.to/3OYb9jk", new Livro("Malorie", "Josh Malerman", 5));
+        livrariaOnline.adicionarLivro("https://amzn.to/45HQE1L", new Livro("E Não Sobrou Nenhum", "Agatha Christie", 50));
+        livrariaOnline.adicionarLivro("https://amzn.to/45u86q4", new Livro("Assassinato no Expresso do Oriente", "Agatha Christie", 5));
+
+        System.out.println("Livros ordenados por preço: \n" + livrariaOnline.exibirLivrosOrdenadosPorPreco());
+
+        System.out.println("Livros ordenados por autor: \n" + livrariaOnline.pesquisarLivrosPorAutor("Josh Malerman"));
+
+        System.out.println("Livro mais caro: " + livrariaOnline.obterLivroMaisCaro());
+
+        System.out.println("Livro mais barato: " + livrariaOnline.obterLivroMaisBarato());
+
+        livrariaOnline.removerLivro("1984");
+        System.out.println(livrariaOnline.livroMap);
+    }
+}
