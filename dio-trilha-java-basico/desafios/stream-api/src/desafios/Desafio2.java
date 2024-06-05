eda841f194b49a1c1685de2546349642778efc8f
gabizelaa <madonamia68@gmail.com>
2024-06-05 14:38:06 -0300

    Adicionando desafio 1 e 2 de StreamAPI

---
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio2.java
@@ -0,0 +1,23 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> numerosPares = numeros.stream()
+                .filter(numero -> numero % 2 == 0)
+                .collect(Collectors.toList());
+
+        int somaPares = numerosPares.stream()
+                .mapToInt(numero -> numero)
+                .sum();
+
+        System.out.println("Soma dos números pares: " + somaPares);
+    }
+}
