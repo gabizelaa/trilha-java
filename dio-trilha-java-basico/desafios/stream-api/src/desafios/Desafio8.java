daefb2392291600cff699158f0890200f3509704
gabizelaa <madonamia68@gmail.com>
2024-06-07 17:29:09 -0300

    Adicionando desafio 8 de StreamAPI

--- dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio8.java
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio8.java
@@ -2,8 +2,6 @@ package desafios;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.IntFunction;
 
 public class Desafio8 {
     public static void main(String[] args) {
