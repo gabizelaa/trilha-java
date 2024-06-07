b830716ebc80e19530972fa9b1d53c2c7cf91e33
gabizelaa <madonamia68@gmail.com>
2024-06-07 16:35:26 -0300

    Adicionando desafio 6 de StreamAPI

--- dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio6.java
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio6.java
@@ -2,8 +2,6 @@ package desafios;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Desafio6 {
     public static void main(String[] args) {
---
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio7.java
@@ -0,0 +1,12 @@
+package desafios;
+
+import java.util.Arrays;
+
+public class Desafio7 {
+    public static void main(String[] args) {
+        Arrays Arrays;
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+    }
+
+
+}
