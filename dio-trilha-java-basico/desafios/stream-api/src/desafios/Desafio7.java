f5ec348205790bbfddd68e14ba293990bb8db973
gabizelaa <madonamia68@gmail.com>
2024-06-07 16:50:51 -0300

    Adicionando desafio 6 e 7 de StreamAPI

--- dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio7.java
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio7.java
@@ -1,11 +1,21 @@
 package desafios;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 public class Desafio7 {
     public static void main(String[] args) {
-        Arrays Arrays;
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    Optional<Integer> segundoMaior = numeros.stream()
+            .sorted(Comparator.reverseOrder())
+                    .limit(2)
+                            .skip(1)
+                                    .findFirst();
+
+        System.out.println(segundoMaior);
     }
 
 
