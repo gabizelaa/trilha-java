e9b25d147131c7fed5efab7eaafd26af2571b70b
gabizelaa <madonamia68@gmail.com>
2024-06-09 19:27:38 -0300

    adicionando desafio 17 sobre StreamAPI

---
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio17.java
@@ -0,0 +1,23 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Desafio17 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Predicate<Integer> isPrime = n -> {
+            if (n <= 1) return false;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        };
+
+        numeros.stream()
+                .filter(isPrime)
+                .forEach(System.out::println);
+    }
+}
