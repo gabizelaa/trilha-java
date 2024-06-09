8fc96eb27b75088c1647f8c58423a4d1e9a12c16
gabizelaa <madonamia68@gmail.com>
2024-06-09 20:12:23 -0300

    atualizando desafio 19 sobre StreamAPI

--- dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio19.java
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio19.java
@@ -1,4 +1,22 @@
 package desafios;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Desafio19 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Predicate<Integer> divisivelPor3e5 = numero -> numero % 3 == 0 && numero % 5 == 0;
+
+        int soma = numeros.stream()
+                .filter(divisivelPor3e5)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println("Soma dos números divisíveis por 3 e 5: " + soma);
+
+    }
 }
