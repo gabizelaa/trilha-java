5966cfa2b610a59be629e3f5163aaa169dc57083
gabizelaa <madonamia68@gmail.com>
2024-06-09 19:53:06 -0300

    atualizando desafio 18 sobre StreamAPI

--- dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio1.java
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio1.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 public class Desafio1 {
     public static void main(String[] args) {
-        // Criar uma lista de números inteiros
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         List<Integer> numerosOrdenados = numeros.stream()
---
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio18.java
@@ -0,0 +1,18 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Desafio18 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean todosIguais = numeros.stream()
+                .distinct()
+                .toList()
+                .size() == 1;
+
+        System.out.println("Todos os números são iguais? " + todosIguais);
+    }
+}
