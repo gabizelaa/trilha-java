00f1fd1713d06f64709e8831d219c47be313b65a
gabizelaa <madonamia68@gmail.com>
2024-06-09 17:08:43 -0300

    adicionando desafios 12 e 13 sobre StreamAPI

---
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio13.java
@@ -0,0 +1,19 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio13 {
+    public static void main(String[] args) {
+        int min = 5;
+        int max = 10;
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+        List<Integer> dentroIntervalo = numeros.stream()
+                .filter(n -> min <= n && n <= max)
+                .toList();
+
+        System.out.println("Números dentro do intervalo [" + min + ", " + max + "]: " + dentroIntervalo);
+    }
+}
