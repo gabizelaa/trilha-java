ec1e4a747028b6536a2806817924441e976a46cf
gabizelaa <madonamia68@gmail.com>
2024-06-09 20:14:38 -0300

    atualizando desafio 19 sobre StreamAPI

--- dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio19.java
+++ dio-trilha-java-basico/desafios/stream-api/src/desafios/Desafio19.java
@@ -3,7 +3,6 @@ package desafios;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Desafio19 {
     public static void main(String[] args) {
