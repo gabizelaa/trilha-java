85fdf04c6a4b14f3f352cd2766e1316b1bf8743f
gabizelaa <madonamia68@gmail.com>
2024-06-13 14:37:51 -0300

    Mentoria e Curso

--- dio-trilha-java-basico/desafios/poo/desafio-poo-2/src/Main.java
+++ dio-trilha-java-basico/desafios/poo/desafio-poo-2/src/Main.java
@@ -1,5 +1,27 @@
+import com.br.dio.desafio.dominio.Curso;
+import com.br.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição curso Javascript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
